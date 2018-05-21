b37bfeb618c0c45b5f71963d56fd720c970b8c91
wuzaofeng <673908452@qq.com>
2018-05-21 17:47:32 +0800

    完成首页渲染

--- src/views/all/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import List from '../../components/list';
-
-export default class Ask extends Component {
-  state = {};
-  render() {
-    return (<List data={this.props.data} />);
-  }
-}
--- src/views/good/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Ask extends Component {
-  state = {};
-  render() {
-    return 'good';
-  }
-}
